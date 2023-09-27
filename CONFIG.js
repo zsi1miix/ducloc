0989d32ee8b698aff13a883af68420c7615b5ed6
zsi1miix <144526970+zsi1miix@users.noreply.github.com>
2023-09-27 20:21:12 +0700

    Add files via upload

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Lô anh tài. </br> Em có DZ ko",
+  descriptionHeader:
+    "Hãy trả lời một cách thật lòng nhé. Nếu anh Tài im lặng và rời đi thì tức là em DZ.",
+  buttonYes: "Có",
+  buttonNo: "Không",
+  titleModar: "Thế thì DC.",
+  descriptionModar: "Lộc DZ"
+    "Cảm ơn vì đả chọn nha. </br> túi mai em bao.</br> tại anh tài sấu trai quá nên em mới bao đó."
+};
